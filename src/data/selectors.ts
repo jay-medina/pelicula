a94c5ec330baf28f48f6e3df3afe1c9b04e86d68
Jose Medina <josemedina921@gmail.com>
2021-02-28 19:53:15 -0800

    add a selector to retrieve the list of movies

---
+++ src/data/selectors.ts
@@ -0,0 +1,27 @@
+import { AppState, Movie } from "./types";
+
+interface GetListOfMoviesPayload {
+    query?: string;
+}
+/**
+ * Returns a list of movies that match the search query
+ * If query is empty, just return the first 100
+ */
+export function getListOfMovies(state: AppState, { query }: GetListOfMoviesPayload): Movie[] {
+    const movies = Object.values(state.movies);
+
+    if (!query) return movies.slice(0, 100);
+
+    // we want to put exact matches first then fuzzy matches
+    const li: Movie[] = [];
+
+    for (const movie of movies) {
+        if (movie.title === query) {
+            li.unshift(movie);
+        } else if (movie.title.includes(query)) {
+            li.push(movie);
+        }
+    }
+
+    return li;
+}
