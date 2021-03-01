7ea18288f12bc4ff016022000bb8a5fb3fd1e3cf
Jose Medina <josemedina921@gmail.com>
2021-02-28 22:01:50 -0800

    move the searcher to thunk and fetchers

--- src/data/thunks.ts
+++ src/data/thunks.ts
@@ -1,15 +1,15 @@
-import { addMovie } from "./actions";
+import { search, searchDetailsById } from "./fetcher";
+import { addMovie, updateMovie } from "./actions";
 import { AppState, ThunkDispatch, Movie } from "./types";
-
-/**
- * This is not the d̶r̶o̶i̶d̶s̶ key you are looking for. *Hand waves*
- */
-const MOVIE_API_KEY = "342dfc75";
-const MOVIE_URL = "http://www.omdbapi.com/";
+import { getMovieRating } from "./selectors";
 
 interface SearchForMoviesPayload {
     query: string;
     page?: number;
+}
+
+interface SearchForMovieByIdPayload {
+    id: string;
 }
 
 interface MovieResponse {
@@ -18,24 +18,6 @@ interface MovieResponse {
     Type: string;
     Year: string;
     imdbID: string;
-}
-
-interface SearchResponse {
-    Response: "True";
-    Search: MovieResponse[];
-    totalResults: string;
-}
-
-async function search({ query, page = 1 }: SearchForMoviesPayload): Promise<SearchResponse> {
-    const urlToSend = `${MOVIE_URL}?apikey=${MOVIE_API_KEY}&s=${query}&page=${page}`;
-
-    const result = await fetch(urlToSend);
-
-    if (result.ok) {
-        return result.json();
-    }
-
-    throw new Error(result.statusText);
 }
 
 // Larger posters are prefixed with 600 and 900
@@ -90,6 +72,30 @@ export function searchForMovies(payload: SearchForMoviesPayload) {
             dispatch(addMovie(movie));
         });
 
+        // if any of them match fetch details
+
         // console.log("results", results);
+    };
+}
+
+export function searchForMovieById(payload: SearchForMovieByIdPayload) {
+    return async (dispatch: ThunkDispatch, getState: () => AppState) => {
+        const state = getState();
+
+        const rating = getMovieRating(state, payload);
+
+        // if we have the rating, we have already fetched this data
+        if (rating) return;
+
+        const data = await searchDetailsById(payload);
+
+        dispatch(
+            updateMovie({
+                id: data.imdbID,
+                title: data.Title,
+                rating: data.Rated,
+                genre: data.Genre,
+            })
+        );
     };
 }
