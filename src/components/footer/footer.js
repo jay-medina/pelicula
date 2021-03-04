163144ea880ae0640b21092290a7af24718ce4f2
Jose Medina <josemedina921@gmail.com>
2021-03-03 23:32:14 -0800

    updates

--- src/components/footer/footer.js
+++
@@ -1,8 +0,0 @@
-import React from "../../../web_modules/react.js";
-export function Footer() {
-  return /* @__PURE__ */ React.createElement("div", {
-    className: "app__footer"
-  }, /* @__PURE__ */ React.createElement("div", null, "PELICULA"), /* @__PURE__ */ React.createElement("div", null, "https://github.com/jayem9000"), /* @__PURE__ */ React.createElement("div", null, "My favorite color is ", /* @__PURE__ */ React.createElement("span", {
-    className: "app__footer_color"
-  }, "Jay-Z blue")));
-}
